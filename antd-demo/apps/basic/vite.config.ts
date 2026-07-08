2abb068e1419778d3acd01eec9fcc16c507420d2
fengli <fengli@wnderent.net>
2026-07-08 16:10:34 +0800

    feat(antd-demo): 更新lingui相关配置

--- antd-demo/apps/basic/vite.config.ts
+++ antd-demo/apps/basic/vite.config.ts
@@ -1,22 +1,24 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { lingui, linguiTransformerBabelPreset } from "@lingui/vite-plugin";
+import { lingui } from "@lingui/vite-plugin";
+import { linguiMacroSwcPlugin } from "@lingui/swc-plugin/options";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    babel({ presets: [reactCompilerPreset()] }),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
     }),
-    react(),
-    lingui(),
-    babel({
-      presets: [linguiTransformerBabelPreset()],
+    // @lingui/swc-plugin 展开成实际的字符串/ICU 格式代码,之后 React Compiler 才能正确分析这段代码的依赖关系、做自动 memoization 优化。
+    react({
+      plugins: [linguiMacroSwcPlugin()],
     }),
+    babel({ presets: [reactCompilerPreset()] }),
+    lingui(),
   ],
   resolve: {
     alias: {
